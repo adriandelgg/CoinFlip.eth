93e1e59669c1f07394d742b8ce240ac81d540538
adriandelgg <adriandelgadoc1@gmail.com>
2021-06-25 18:23:48 -0700

    First commit

---
+++ hardhat.config.js
@@ -0,0 +1,43 @@
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task('accounts', 'Prints the list of accounts', async () => {
+	const accounts = await ethers.getSigners();
+
+	for (const account of accounts) {
+		console.log(account.address);
+	}
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+	networks: {
+		rinkeby: {
+			url: process.env.INFURA_RINKEBY_KEY,
+			accounts: [process.env.PRIVATE_KEY_17]
+		},
+		kovan: {
+			url: process.env.INFURA_KOVAN_KEY,
+			accounts: [process.env.PRIVATE_KEY_17]
+		},
+		ropsten: {
+			url: process.env.INFURA_ROPSTEN_KEY,
+			accounts: [process.env.PRIVATE_KEY_17]
+		}
+	},
+	solidity: {
+		version: '0.8.5',
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 200
+			}
+		}
+	}
+};
---
+++ test/sample-test.js
@@ -0,0 +1,14 @@
+const { expect } = require('chai');
+
+describe('Greeter', function () {
+	let Minter, owner, alice, bob, contract, token1, token2;
+
+	beforeEach(async function () {
+		Minter = await ethers.getContractFactory('Minter');
+		[owner, alice, bob] = await ethers.getSigners();
+		contract = await Minter.deploy();
+		contract2 = contract.connect(alice);
+	});
+
+	describe('', () => {});
+});
