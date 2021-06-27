e6b3188d9d3eb2828c1e33f2fcc3a92d7ef09f1e
adriandelgg <adriandelgadoc1@gmail.com>
2021-06-27 00:42:31 -0700

    Tested more functions and added player 2 bet in

--- contracts/CoinFlip.sol
+++ contracts/CoinFlip.sol
@@ -18,6 +18,9 @@ contract CoinFlip is ERC20 {
     // Mapping from game ID to bet amount
     mapping(uint256 => uint256) private _betAmount;
 
+    // Mapping from game ID to player that gets to flip coin
+    mapping(uint256 => address) private _coinFlipper;
+
     function currentGameID() public returns (uint256) {
         return _gameID;
     }
@@ -28,12 +31,20 @@ contract CoinFlip is ERC20 {
         view
         returns (address)
     {
-        require(_player == 0 || _player == 1, "Number must be 0 or 1");
+        require(_player == 1 || _player == 2, "Number must be 1 or 2");
         require(_id < _gameID, "Game doesn't exist");
-        if (_player == 0) {
+        if (_player == 1) {
             return _player1[_id];
         } else {
             return _player2[_id];
+        }
+    }
+
+    function setCoinFlipper(uint256 _id, uint256 _random) private {
+        if (_random == 1) {
+            _coinFlipper[_id] = _player1[_id];
+        } else {
+            _coinFlipper[_id] = _player2[_id];
         }
     }
 
@@ -43,10 +54,6 @@ contract CoinFlip is ERC20 {
     function getTokens(uint256 _amount) public {
         require(_amount < totalSupply(), "Amount is more than total supply.");
         _transfer(address(this), msg.sender, _amount);
-    }
-
-    function selectPlayer(uint256 _player) public {
-        require(_player == 0 || _player == 1, "Number must be 0 or 1");
     }
 
     // Creates a new game with the player initializing the amount
@@ -62,18 +69,27 @@ contract CoinFlip is ERC20 {
         _gameID++;
     }
 
-    function betTokens(uint256 _amount, uint256 _id) public {
+    function betTokens(
+        uint256 _amount,
+        uint256 _id,
+        uint256 _random
+    ) public {
         require(
             _amount <= balanceOf(msg.sender) && _amount >= 2e12,
             "Not enough or too many tokens"
         );
-        require(_id <= _gameID, "Game doesn't exist!");
-        require(_player2[_id] != address(0), "2nd player already exists");
-        require(_amount <= _betAmount[_id], "Amount not equal to bet amount");
+        require(_id < _gameID, "Game doesn't exist!");
+        require(_random == 1 || _random == 2, "Random number must be 1 or 2");
+        require(_player2[_id] == address(0), "2nd player already exists");
+        require(_amount == _betAmount[_id], "Amount not equal to bet amount");
+        transfer(address(this), _amount);
         _player2[_id] = msg.sender;
+        setCoinFlipper(_id, _random);
     }
 
-    function startGame() public {}
+    // function startGame() public {
+    //     require()
+    // }
 
     // Let player put funds in for a game
     // Funds can be random, but must equal each other from both players
--- test/CoinFlip.test.js
+++ test/CoinFlip.test.js
@@ -1,12 +1,12 @@
 const { expect } = require('chai');
 
 describe('CoinFlip', function () {
-	let CoinFlip, owner, alice, bob, contract, token1, token2;
+	let CoinFlip, owner, alice, contract, contract2;
 	const oneEther = ethers.BigNumber.from('1000000000000000000');
 
 	beforeEach(async function () {
 		CoinFlip = await ethers.getContractFactory('CoinFlip');
-		[owner, alice, bob] = await ethers.getSigners();
+		[owner, alice] = await ethers.getSigners();
 		contract = await CoinFlip.deploy();
 		contract2 = contract.connect(alice);
 	});
@@ -32,35 +32,80 @@ describe('CoinFlip', function () {
 		expect(contractAmountBefore).to.be.greaterThan(contractAmountAfter);
 	});
 
-	it('should revert if player is not 0 or 1', async () => {
-		await expect(contract.selectPlayer(7)).to.be.reverted;
-		await expect(contract.selectPlayer(2)).to.be.revertedWith(
-			'Number must be 0 or 1'
-		);
-		await expect(contract.selectPlayer(0)).to.not.be.reverted;
-		await expect(contract.selectPlayer(1)).to.not.be.reverted;
+	describe('Setting up Player 1 & Game', async function () {
+		it('should revert if player is not 1 or 2', async () => {
+			await expect(contract.getPlayerAddress(7, 0)).to.be.reverted;
+			await expect(contract.getPlayerAddress(3, 0)).to.be.revertedWith(
+				'Number must be 1 or 2'
+			);
+		});
+
+		it("should return correct player's address", async () => {
+			await contract.getTokens(oneEther);
+			await contract.createGame(3e12);
+			expect(await contract.getPlayerAddress(1, 0)).to.equal(owner.address);
+
+			const address = ethers.utils.hexStripZeros(
+				await contract.getPlayerAddress(2, 0)
+			);
+			expect(address).to.equal('0x');
+		});
+
+		it('should create a new game & set values properly to mappings', async () => {
+			// Checks that tokens were transferred to caller
+			await contract.getTokens(oneEther);
+			await expect(contract.createGame(1e12)).to.be.reverted;
+
+			expect(await contract.balanceOf(owner.address)).to.equal(oneEther);
+
+			// Checks that contract received collateral
+			const contractAmountBefore = await contract.balanceOf(contract.address);
+			await contract.createGame(3e12);
+			const contractAmountAfter = await contract.balanceOf(contract.address);
+
+			expect(contractAmountBefore.lt(contractAmountAfter)).to.be.true;
+
+			const player1 = await contract.getPlayerAddress(1, 0);
+			expect(player1).to.equal(owner.address);
+		});
 	});
 
-	it("should return correct player's address", async () => {
-		// expect(await contract.getPlayerAddress(0, 0)).to.equal('0x0');
-		await contract.getTokens(oneEther);
-		await contract.createGame(3e12);
+	describe('Setting up Player 2', () => {
+		beforeEach(async function () {
+			await contract.getTokens(oneEther);
+			await contract2.getTokens(oneEther);
+			await contract.createGame(3e12);
+			randomNum = Math.floor(Math.random() * 2) + 1;
+		});
 
-		console.log(await contract.getPlayerAddress(0, 0));
-	});
+		it('should pass all betTokens() requires', async () => {
+			await expect(contract2.betTokens(4e12, 0, randomNum)).to.be.revertedWith(
+				'Amount not equal to bet amount'
+			);
+			await contract2.betTokens(3e12, 0, randomNum);
+			await expect(contract2.betTokens(3e12, 1, randomNum)).to.be.revertedWith(
+				"Game doesn't exist!"
+			);
+			await expect(contract2.betTokens(3e12, 0, randomNum)).to.be.revertedWith(
+				'2nd player already exists'
+			);
+			await expect(
+				contract2.betTokens(oneEther, 0, randomNum)
+			).to.be.revertedWith('Not enough or too many tokens');
+			await expect(contract2.betTokens(1, 0, randomNum)).to.be.revertedWith(
+				'Not enough or too many tokens'
+			);
+		});
 
-	it('should create a new game & set values properly to mappings', async () => {
-		// Checks that tokens were transferred to caller
-		await contract.getTokens(oneEther);
-		await expect(contract.createGame(1e12)).to.be.reverted;
+		it('should allow 2nd player to bet in', async () => {
+			const contractAmountBefore = await contract2.balanceOf(contract2.address);
+			await contract2.betTokens(3e12, 0, randomNum);
+			const contractAmountAfter = await contract2.balanceOf(contract2.address);
 
-		expect(await contract.balanceOf(owner.address)).to.equal(oneEther);
+			expect(contractAmountBefore.lt(contractAmountAfter)).to.be.true;
 
-		// Checks that contract received collateral
-		const contractAmountBefore = await contract.balanceOf(contract.address);
-		await contract.createGame(3e12);
-		const contractAmountAfter = await contract.balanceOf(contract.address);
-
-		expect(contractAmountBefore.lt(contractAmountAfter)).to.be.true;
+			const player2 = await contract2.getPlayerAddress(2, 0);
+			expect(player2).to.equal(alice.address);
+		});
 	});
 });
